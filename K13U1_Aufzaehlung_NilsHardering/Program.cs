540f4d1f78d1c84135a3551ea4f96140ab4f5501
cherry <hardering64@hotmail.com>
2021-01-08 18:43:41 +0100

    Add project files.

---
+++ K13U1_Aufzaehlung_NilsHardering/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace K13U1_Aufzaehlung_NilsHardering
+{
+    enum Farben
+    {
+        Rot,
+
+        Gelb,
+
+        Grün,
+
+        Blau
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string s in Enum.GetNames(typeof(Farben)))
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
